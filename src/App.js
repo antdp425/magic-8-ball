3c4e52808630003aef6b2bc15b1261a5bb4aa108
antdp425 <antdp425@aol.com>
2021-03-21 16:13:14 -0400

    adds main 8ball image

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Milk</h1>
+      <h1>Magic 8-Ball</h1>
+      <img className="main8BallImage" src="./magic8ball.jpeg" alt="Magic 8Ball"></img>
     </div>
   );
 }
