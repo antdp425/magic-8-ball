b7a404f278f19f981e0c694934821d9b122b6bce
antdp425 <antdp425@aol.com>
2021-03-21 17:02:22 -0400

    adds UI elements and starter CSS

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ function App() {
     <div className="App">
       <h1>Magic 8-Ball</h1>
       <img className="main8BallImage" src="./magic8ball.jpeg" alt="Magic 8Ball"></img>
+      <p id="queryResult"></p>
+      <div id="queryInput">
+          <input id="query" type="text"></input>
+          <button id="getQuery">Ask</button>
+      </div>
+      <div id="history">
+        <button>Show History</button>
+      </div>
     </div>
   );
 }
