c949892d37c684e8293318c295e624fd5812e907
antdp425 <antdp425@aol.com>
2021-03-21 17:25:25 -0400

    incorporates color scheme + addt'l CSS

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <h1>Magic 8-Ball</h1>
-      <img className="main8BallImage" src="./magic8ball.jpeg" alt="Magic 8Ball"></img>
+      <img
+        className="main8BallImage"
+        src="./magic8ball.jpeg"
+        alt="Magic 8Ball"
+      ></img>
       <p id="queryResult"></p>
-      <div id="queryInput">
-          <input id="query" type="text"></input>
-          <button id="getQuery">Ask</button>
-      </div>
+      <form id="queryInput">
+        <input id="query" type="text"></input>
+        <input id="getResult" type="submit" value="Ask"></input>
+      </form>
       <div id="history">
         <button>Show History</button>
       </div>
